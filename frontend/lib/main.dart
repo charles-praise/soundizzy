78e63af8272fb2f6523f059d197c5784b2e53133
charles-praise <charlespraise2001@gmail.com>
2025-08-18 02:13:06 +0100

    fixing bugs

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,15 +1,7 @@
-import 'export.dart';
+import 'package:flutter/material.dart';
 
 void main(List<String> args) {
-  runApp(
-    MultiBlocProvider(
-      providers: [
-        BlocProvider(create: (context) => AuthCubit()),
-        BlocProvider(create: (context) => UploadCubit()),
-      ],
-      child: const Soundizzy(),
-    ),
-  );
+  runApp(const Soundizzy());
 }
 
 // ----------- Soundizzy (Main) ----------------
@@ -18,57 +10,10 @@ class Soundizzy extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Soundizzy',
-      color: AppColors.transparentBackground,
-      routes: {
-        '/discover': (context) => const Discover(),
-        '/marketplace': (context) => const Marketplace(),
-        '/search': (context) => const AppSearch(),
-        'profile': (context) => const Profile(),
-        '/view': (context) => const AppView(),
-      },
-      theme: AppTheme.lightTheme,
-      home: SafeArea(child: const Splash()),
+      home: SafeArea(child: Scaffold()),
       debugShowCheckedModeBanner: false,
-    );
-  }
-}
-
-// ---------- Splash screen -----------
-class Splash extends StatelessWidget {
-  const Splash({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: BlocListener<AuthCubit, AuthState>(
-        listener: (context, state) {
-          if (state is AuthSuccess) {
-            print('success');
-          }
-        },
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Expanded(
-              child: Center(
-                child: Text(
-                  'Soundizzy initializing .....',
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 23,
-                      fontStyle: FontStyle.italic),
-                ),
-              ),
-            ),
-            LinearProgressIndicator(
-              minHeight: 10,
-              color: AppColors.LinearProgressIndicatorLightMode,
-            )
-          ],
-        ),
-      ),
     );
   }
 }
